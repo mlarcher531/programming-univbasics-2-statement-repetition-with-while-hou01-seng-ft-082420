f8ddc7f0bf749130a0d43b4b66340aefa341344a
Mike Archer <mlarcher531@gmail.com>
2020-07-24 15:26:25 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,8 +1,8 @@
 magic_exit_number = 0
 count = 10
-while count <= 10 do
+while count >= 1 do
   break if count == magic_exit_number
   puts count
-  count = count - 1
+  count -= 1
 end
 puts = "Happy New Year!"
